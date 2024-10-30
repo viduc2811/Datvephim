4df5c48abdc7365143f9b6d4113b35c1efd585af
viduc2811 <viduc28112002@gmail.com>
2024-10-30 23:46:36 +0700

    1st commit

---
+++ rapphim1/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿
+namespace rapphim1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cboMovies = new System.Windows.Forms.ComboBox();
+            this.btnThanhtoan = new System.Windows.Forms.Button();
+            this.lblTotalAmount = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.A = new System.Windows.Forms.Button();
+            this.B = new System.Windows.Forms.Button();
+            this.C = new System.Windows.Forms.Button();
+            this.D = new System.Windows.Forms.Button();
+            this.E = new System.Windows.Forms.Button();
+            this.F = new System.Windows.Forms.Button();
+            this.G = new System.Windows.Forms.Button();
+            this.H = new System.Windows.Forms.Button();
+            this.I = new System.Windows.Forms.Button();
+            this.J = new System.Windows.Forms.Button();
+            this.K = new System.Windows.Forms.Button();
+            this.L = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cboMovies
+            // 
+            this.cboMovies.FormattingEnabled = true;
+            this.cboMovies.Location = new System.Drawing.Point(279, 74);
+            this.cboMovies.Name = "cboMovies";
+            this.cboMovies.Size = new System.Drawing.Size(121, 21);
+            this.cboMovies.TabIndex = 8;
+            // 
+            // btnThanhtoan
+            // 
+            this.btnThanhtoan.Location = new System.Drawing.Point(574, 363);
+            this.btnThanhtoan.Name = "btnThanhtoan";
+            this.btnThanhtoan.Size = new System.Drawing.Size(75, 23);
+            this.btnThanhtoan.TabIndex = 9;
+            this.btnThanhtoan.Text = "Thanh toán";
+            this.btnThanhtoan.UseVisualStyleBackColor = true;
+            this.btnThanhtoan.Click += new System.EventHandler(this.btnThanhtoan_Click);
+            // 
+            // lblTotalAmount
+            // 
+            this.lblTotalAmount.AutoSize = true;
+            this.lblTotalAmount.Location = new System.Drawing.Point(72, 363);
+            this.lblTotalAmount.Name = "lblTotalAmount";
+            this.lblTotalAmount.Size = new System.Drawing.Size(58, 13);
+            this.lblTotalAmount.TabIndex = 10;
+            this.lblTotalAmount.Text = "Thành tiền";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 6;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 53.06123F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 46.93877F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 94F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 91F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 55F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel1.Controls.Add(this.A, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.B, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.C, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.D, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.E, 4, 0);
+            this.tableLayoutPanel1.Controls.Add(this.F, 5, 0);
+            this.tableLayoutPanel1.Controls.Add(this.G, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.H, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.I, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.J, 3, 1);
+            this.tableLayoutPanel1.Controls.Add(this.K, 4, 1);
+            this.tableLayoutPanel1.Controls.Add(this.L, 5, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(123, 115);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(468, 118);
+            this.tableLayoutPanel1.TabIndex = 12;
+            // 
+            // A
+            // 
+            this.A.Location = new System.Drawing.Point(3, 3);
+            this.A.Name = "A";
+            this.A.Size = new System.Drawing.Size(72, 23);
+            this.A.TabIndex = 0;
+            this.A.Text = "A";
+            this.A.UseVisualStyleBackColor = true;
+            // 
+            // B
+            // 
+            this.B.Location = new System.Drawing.Point(81, 3);
+            this.B.Name = "B";
+            this.B.Size = new System.Drawing.Size(63, 23);
+            this.B.TabIndex = 1;
+            this.B.Text = "B";
+            this.B.UseVisualStyleBackColor = true;
+            // 
+            // C
+            // 
+            this.C.Location = new System.Drawing.Point(150, 3);
+            this.C.Name = "C";
+            this.C.Size = new System.Drawing.Size(75, 23);
+            this.C.TabIndex = 2;
+            this.C.Text = "C";
+            this.C.UseVisualStyleBackColor = true;
+            // 
+            // D
+            // 
+            this.D.Location = new System.Drawing.Point(244, 3);
+            this.D.Name = "D";
+            this.D.Size = new System.Drawing.Size(75, 23);
+            this.D.TabIndex = 3;
+            this.D.Text = "D";
+            this.D.UseVisualStyleBackColor = true;
+            // 
+            // E
+            // 
+            this.E.Location = new System.Drawing.Point(335, 3);
+            this.E.Name = "E";
+            this.E.Size = new System.Drawing.Size(49, 23);
+            this.E.TabIndex = 4;
+            this.E.Text = "E";
+            this.E.UseVisualStyleBackColor = true;
+            // 
+            // F
+            // 
+            this.F.Location = new System.Drawing.Point(390, 3);
+            this.F.Name = "F";
+            this.F.Size = new System.Drawing.Size(75, 23);
+            this.F.TabIndex = 5;
+            this.F.Text = "F";
+            this.F.UseVisualStyleBackColor = true;
+            // 
+            // G
+            // 
+            this.G.Location = new System.Drawing.Point(3, 62);
+            this.G.Name = "G";
+            this.G.Size = new System.Drawing.Size(72, 23);
+            this.G.TabIndex = 6;
+            this.G.Text = "G";
+            this.G.UseVisualStyleBackColor = true;
+            // 
+            // H
+            // 
+            this.H.Location = new System.Drawing.Point(81, 62);
+            this.H.Name = "H";
+            this.H.Size = new System.Drawing.Size(63, 23);
+            this.H.TabIndex = 7;
+            this.H.Text = "H";
+            this.H.UseVisualStyleBackColor = true;
+            // 
+            // I
+            // 
+            this.I.Location = new System.Drawing.Point(150, 62);
+            this.I.Name = "I";
+            this.I.Size = new System.Drawing.Size(75, 23);
+            this.I.TabIndex = 8;
+            this.I.Text = "I";
+            this.I.UseVisualStyleBackColor = true;
+            // 
+            // J
+            // 
+            this.J.Location = new System.Drawing.Point(244, 62);
+            this.J.Name = "J";
+            this.J.Size = new System.Drawing.Size(75, 23);
+            this.J.TabIndex = 9;
+            this.J.Text = "J";
+            this.J.UseVisualStyleBackColor = true;
+            // 
+            // K
+            // 
+            this.K.Location = new System.Drawing.Point(335, 62);
+            this.K.Name = "K";
+            this.K.Size = new System.Drawing.Size(49, 23);
+            this.K.TabIndex = 10;
+            this.K.Text = "K";
+            this.K.UseVisualStyleBackColor = true;
+            // 
+            // L
+            // 
+            this.L.Location = new System.Drawing.Point(390, 62);
+            this.L.Name = "L";
+            this.L.Size = new System.Drawing.Size(75, 23);
+            this.L.TabIndex = 11;
+            this.L.Text = "L";
+            this.L.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.lblTotalAmount);
+            this.Controls.Add(this.btnThanhtoan);
+            this.Controls.Add(this.cboMovies);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ComboBox cboMovies;
+        private System.Windows.Forms.Button btnThanhtoan;
+        private System.Windows.Forms.Label lblTotalAmount;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button A;
+        private System.Windows.Forms.Button B;
+        private System.Windows.Forms.Button C;
+        private System.Windows.Forms.Button D;
+        private System.Windows.Forms.Button E;
+        private System.Windows.Forms.Button F;
+        private System.Windows.Forms.Button G;
+        private System.Windows.Forms.Button H;
+        private System.Windows.Forms.Button I;
+        private System.Windows.Forms.Button J;
+        private System.Windows.Forms.Button K;
+        private System.Windows.Forms.Button L;
+    }
+}
+
---
+++ rapphim1/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace rapphim1
+{
+    public partial class Form1 : Form
+    {
+        private Dictionary<Button, int> seatPrices = new Dictionary<Button, int>();
+        private int totalAmount = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeSeats();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void InitializeSeats()
+        {
+            // Gán giá cho từng hàng ghế
+            AssignSeatPrice(A, 25000);
+            AssignSeatPrice(B, 25000);
+            AssignSeatPrice(C, 23000);
+            AssignSeatPrice(D, 25000);
+            AssignSeatPrice(E, 13000);
+            AssignSeatPrice(F, 25000);
+            AssignSeatPrice(G, 25000);
+            AssignSeatPrice(H, 25000);
+            AssignSeatPrice(I, 25000);
+            AssignSeatPrice(J, 30000);
+            AssignSeatPrice(K, 25000);
+            AssignSeatPrice(L, 25000);
+            
+            // Gán sự kiện Click cho tất cả các ghế
+            foreach (var seat in seatPrices.Keys)
+            {
+                seat.Click += Seat_Click;
+            }
+        }
+
+        private void AssignSeatPrice(Button seat, int price)
+        {
+            seatPrices[seat] = price;
+            seat.MouseHover += (s, e) =>
+            {
+                ToolTip tooltip = new ToolTip();
+                tooltip.Show($"Giá vé của hàng ghế này là: {price}đ", seat, 1000);
+            };
+        }
+
+        private void Seat_Click(object sender, EventArgs e)
+        {
+            Button seat = sender as Button;
+
+            if (seat.BackColor == Color.Green)
+            {
+                // Hủy chọn ghế
+                seat.BackColor = SystemColors.Control;
+                totalAmount -= seatPrices[seat];
+            }
+            else
+            {
+                // Chọn ghế
+                seat.BackColor = Color.Green;
+                totalAmount += seatPrices[seat];
+            }
+
+            UpdateTotalAmount();
+        }
+
+        private void UpdateTotalAmount()
+        {
+            lblTotalAmount.Text = $"Thành tiền: {totalAmount}đ";
+        }
+
+        private void btnThanhtoan_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Bạn có chắc chắn muốn mua những ghế đã chọn không?", "Xác nhận thanh toán", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                MessageBox.Show("Thanh toán thành công!", "Thông báo");
+                // Reset ghế và tổng tiền
+                foreach (var seat in seatPrices.Keys)
+                {
+                    if (seat.BackColor == Color.Green)
+                    {
+                        seat.BackColor = Color.Red;
+                    }
+                }
+                totalAmount = 0;
+                UpdateTotalAmount();
+            }
+        }
+    }
+}
